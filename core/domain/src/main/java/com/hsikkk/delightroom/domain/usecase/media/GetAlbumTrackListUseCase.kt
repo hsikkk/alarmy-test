59723a5413e4024f314cec9d139a30ccff125ee0
hsikkk <gcorp14@naver.com>
2024-03-01 17:10:54 +0900

    feat: Album, Track 목록 가져올 UseCase 클래스 생성 - 내부 구현은 TODO

---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/usecase/media/GetAlbumTrackListUseCase.kt
@@ -0,0 +1,11 @@
+package com.hsikkk.delightroom.domain.usecase.media
+
+import com.hsikkk.delightroom.domain.model.entity.Track
+
+class GetAlbumTrackListUseCase(
+
+) {
+    fun invoke(albumId: Long) : List<Track> {
+        TODO()
+    }
+}
---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/usecase/media/GetAlbumsUseCase.kt
@@ -0,0 +1,11 @@
+package com.hsikkk.delightroom.domain.usecase.media
+
+import com.hsikkk.delightroom.domain.model.entity.Album
+
+class GetAlbumsUseCase(
+
+) {
+    fun invoke() : List<Album> {
+        TODO()
+    }
+}
