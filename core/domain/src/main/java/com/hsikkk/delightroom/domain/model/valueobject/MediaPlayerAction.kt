f6dc0288950ac89335ec02b6d0e628fafd84ab52
hsikkk <gcorp14@naver.com>
2024-03-02 14:50:40 +0900

    feat: mediaPlayer 제어 동작 sealedInterface로 정의 - 변동 가능

---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/model/valueobject/MediaPlayerAction.kt
@@ -0,0 +1,29 @@
+package com.hsikkk.delightroom.domain.model.valueobject
+
+import com.hsikkk.delightroom.domain.model.entity.Track
+
+sealed interface MediaPlayerAction {
+    object Prepare : MediaPlayerAction
+
+    object Play : MediaPlayerAction
+
+    object Pause : MediaPlayerAction
+
+    data class AddItemsToPlaylist(
+        val items: List<Track>,
+        val shuffleRandom: Boolean,
+        val startIndex: Int,
+    ) : MediaPlayerAction
+
+    object GoNext : MediaPlayerAction
+
+    object GoPrev : MediaPlayerAction
+
+    data class SeekTo(
+        val progress: Float
+    ) : MediaPlayerAction
+
+    data class ChangeVolume(
+        val volume: Float
+    ): MediaPlayerAction
+}
