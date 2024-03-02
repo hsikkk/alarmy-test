ed8394f289c655a850b2f346ca851fb059cc6bbc
hsikkk <gcorp14@naver.com>
2024-03-02 15:58:38 +0900

    feat: mediaPlayer 관련 di 세팅 - player : ExoPlayer 사용

--- core/di/src/main/java/com/hsikkk/delightroom/di/DataSourceModule.kt
+++ core/di/src/main/java/com/hsikkk/delightroom/di/DataSourceModule.kt
@@ -1,8 +1,11 @@
 package com.hsikkk.delightroom.di
 
 import android.content.Context
+import androidx.media3.common.Player
 import com.hsikkk.delightroom.data.datasource.LocalMediaDataSource
+import com.hsikkk.delightroom.data.datasource.LocalMediaPlayerDataSource
 import com.hsikkk.delightroom.datasource.media.LocalMediaDataSourceImpl
+import com.hsikkk.delightroom.datasource.media.LocalMediaPlayerDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,6 +23,16 @@ object DataSourceModule {
     ) : LocalMediaDataSource {
         return LocalMediaDataSourceImpl(
             context = context
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideLocalMediaPlayerDataSource(
+        player: Player
+    ) : LocalMediaPlayerDataSource {
+        return LocalMediaPlayerDataSourceImpl(
+            player = player
         )
     }
 }
