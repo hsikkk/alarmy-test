01358831d80102b7c50de4f611c9c2c2230b6f7f
hsikkk <gcorp14@naver.com>
2024-03-02 14:57:58 +0900

    feat: RequestMediaPlayerActionUseCase 클래스 생성 - 내부 구현 todo

---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/usecase/media/RequestMediaPlayerActionUseCase.kt
@@ -0,0 +1,11 @@
+package com.hsikkk.delightroom.domain.usecase.media
+
+import com.hsikkk.delightroom.domain.model.valueobject.MediaPlayerAction
+
+class RequestMediaPlayerActionUseCase(
+
+) {
+    operator fun invoke(action: MediaPlayerAction){
+        //TODO
+    }
+}
