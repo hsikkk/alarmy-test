bb7c5a0822a06e9d820b666838c0516f40687666
hsikkk <gcorp14@naver.com>
2024-03-02 14:55:39 +0900

    feat: mediaPlayer status 클래스 정의 - 내부 필드 변경 가능

---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/model/valueobject/MediaPlayerStatus.kt
@@ -0,0 +1,19 @@
+package com.hsikkk.delightroom.domain.model.valueobject
+
+import com.hsikkk.delightroom.domain.model.entity.Track
+
+data class MediaPlayerStatus(
+    val isInPlaying: Boolean,
+
+    val currentTrackIndex: Int,
+    val playList: List<Track>,
+
+    val progress: Float,
+
+    val volume: Float,
+){
+    val currentTrack: Track?
+        get() = playList.getOrNull(currentTrackIndex)
+    val duration: Int?
+        get() = currentTrack?.duration
+}
