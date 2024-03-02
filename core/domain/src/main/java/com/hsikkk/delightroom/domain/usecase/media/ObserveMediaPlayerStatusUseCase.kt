eac3b6b73d86f87e77bc5ed46d436f7c9f8d2a1d
hsikkk <gcorp14@naver.com>
2024-03-02 15:02:34 +0900

    feat: ObserveMediaPlayerStatusUseCase 클래스 생성 - 내부 구현 todo - player 상태 변경 마다 emit되는 flow 반환 - domain 모듈 내 coroutine 의존성 추가

---
+++ core/domain/src/main/java/com/hsikkk/delightroom/domain/usecase/media/ObserveMediaPlayerStatusUseCase.kt
@@ -0,0 +1,12 @@
+package com.hsikkk.delightroom.domain.usecase.media
+
+import com.hsikkk.delightroom.domain.model.valueobject.MediaPlayerStatus
+import kotlinx.coroutines.flow.Flow
+
+class ObserveMediaPlayerStatusUseCase (
+
+){
+    operator fun invoke() : Flow<MediaPlayerStatus>{
+        TODO()
+    }
+}
