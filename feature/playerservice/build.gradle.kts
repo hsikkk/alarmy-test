c647eacaa86f42c0254ad8f48e20f33c60d4253f
hsikkk <gcorp14@naver.com>
2024-03-02 16:06:52 +0900

    feat: BackGround 재생 및 외부 제어를 위한 PlayerService 모듈 추가

---
+++ feature/playerservice/build.gradle.kts
@@ -0,0 +1,47 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.hsikkk.delightroom.playerservice"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.media3.common)
+    implementation(libs.media3.session)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
