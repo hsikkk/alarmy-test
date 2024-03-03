579c81c58193315858566112ddc71a8b6605426a
hsikkk <61226036+hsikkk@users.noreply.github.com>
2024-03-03 11:38:23 +0900

    feat: ui 공통으로 사용할 BaseViewModel, Navigation 관련한 Common 모듈 생성 (#6)
    * feat: common 모듈 생성
    
    * feat: viewModel base class 생성
    
    * feat: navigation 관련 공통 클래스 작성

---
+++ core/common/src/main/java/com/hsikkk/delightroom/common/viewmodel/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package com.hsikkk.delightroom.common.viewmodel
+
+import androidx.lifecycle.ViewModel
+import org.orbitmvi.orbit.ContainerHost
+
+abstract class BaseViewModel<Intent: Any, STATE : Any, SIDE_EFFECT : Any>
+    : ViewModel(), ContainerHost<STATE, SIDE_EFFECT> {
+
+    // View에서는 onIntent를 통해서만 이벤트 전달
+    fun onIntent(intent: Intent){
+        handleIntent(intent)
+    }
+
+    protected abstract fun handleIntent(intent: Intent)
+}
