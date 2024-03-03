b6784d4d501a8a0b16ca20313f1f5d5f5cf72362
hsikkk <gcorp14@naver.com>
2024-03-03 12:15:19 +0900

    feat: album 화면에 필요한 contract 정의

---
+++ feature/browser/ui/src/main/java/com/hsikkk/delightroom/browser/ui/contract/AlbumIntent.kt
@@ -0,0 +1,17 @@
+package com.hsikkk.delightroom.browser.ui.contract
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+
+sealed interface AlbumIntent {
+    @Stable
+    object Initialize : AlbumIntent
+
+    @Stable
+    object GoBack : AlbumIntent
+
+    @Immutable
+    data class GoAlbumDetail(
+        val albumId: Long
+    ) : AlbumIntent
+}
