93f365a96b7f93521a2da949e13257343a4233ec
hsikkk <gcorp14@naver.com>
2024-03-04 22:28:08 +0900

    fix: scaffold mini player 보여질 때 하단 padding 추가

--- feature/player/src/main/java/com/hsikkk/delightroom/player/PlayerBottomSheetScaffold.kt
+++ feature/player/src/main/java/com/hsikkk/delightroom/player/PlayerBottomSheetScaffold.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.BottomSheetScaffoldState
@@ -101,7 +102,10 @@ private fun PlayerBottomSheetScaffoldBody(
 
     BottomSheetScaffold(
         content = {
-            Box {
+            Box(
+                modifier = Modifier.fillMaxSize()
+                    .padding(bottom = sheetPeekHeight)
+            ) {
                 content()
 
                 if (isExpanded) {
