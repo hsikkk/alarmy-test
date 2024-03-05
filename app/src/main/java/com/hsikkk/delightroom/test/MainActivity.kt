3735f3e22e4a28fec72d72b9b6089136be909d0e
hsikkk <gcorp14@naver.com>
2024-03-05 18:16:00 +0900

    fix: player bottomsheet dimmed 영역 status Bar 까지 확장

--- app/src/main/java/com/hsikkk/delightroom/test/MainActivity.kt
+++ app/src/main/java/com/hsikkk/delightroom/test/MainActivity.kt
@@ -2,7 +2,14 @@ package com.hsikkk.delightroom.test
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.hsikkk.delightroom.designsystem.theme.DelightroomtestTheme
 import com.hsikkk.delightroom.player.PlayerBottomSheetScaffold
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,10 +18,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.dark(Color(0f,0f,0f).toArgb())
+        )
         setContent {
             DelightroomtestTheme {
-                PlayerBottomSheetScaffold {
-                    AppNavHost()
+                Box(modifier = Modifier.navigationBarsPadding()){
+                    PlayerBottomSheetScaffold() {
+                        AppNavHost()
+                    }
                 }
             }
         }
--- core/designsystem/src/main/java/com/hsikkk/delightroom/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/hsikkk/delightroom/designsystem/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -40,7 +39,6 @@ fun DelightroomtestTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
